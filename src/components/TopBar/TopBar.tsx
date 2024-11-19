a878c17758b6a037e14f49d11ab0adcecc3e3b9f
Majid2Dev <gorginmajid@gmail.com>
2024-11-19 22:01:29 +0330

    fix top bar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,20 +1,9 @@
 import { MoonIcon } from "@heroicons/react/24/outline";
-import Paragraph from "../typography/Paragraph";
 import ProfileBox from "../avatar/ProfileBox";
 import Bell from "../bell/Bell";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { routesList } from "../../utils/routesList";
 
 const TopBar = () => {
-  const { pathname } = useLocation();
-  const [pageTitle, setPageTitle] = useState<string>("داشبورد");
-  useEffect(() => {
-    const findCurrentPage = routesList.find((i) => i.path === pathname);
-    if (findCurrentPage) {
-      setPageTitle(findCurrentPage.title);
-    }
-  }, [pathname]);
+
   return (
     <div className="bg-white flex md:flex-row flex-col-reverse justify-between gap-4 flex-wrap items-center shadow-md  p-4 w-full sticky top-0 z-40">
       <div className="md:block hidden"></div>
