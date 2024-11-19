8c0ee5eac04f5f4006e6f77b455e4c74fc751e32
Majid <73994155+Majid2Dev@users.noreply.github.com>
2024-11-19 22:34:45 +0330

    push

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "./ThemeToggle";
 const TopBar = () => {
 
   return (
-    <div className="light:bg-white   flex md:flex-row flex-col-reverse justify-between gap-4 flex-wrap items-center shadow-md  p-4 w-full sticky top-0 z-40">
+    <div className="bg-white   flex md:flex-row flex-col-reverse justify-between gap-4 flex-wrap items-center shadow-md  p-4 w-full sticky top-0 z-40">
       <div className="md:block hidden"></div>
       <div className="md:w-fit w-full flex items-center md:justify-end justify-between gap-4 flex-wrap">
         <Bell />
