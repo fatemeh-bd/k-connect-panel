4e5ca2216f9969091876f3308cdc589074ac13fb
fatemehbd <baridaryan@gmail.com>
2024-11-22 21:08:18 +0330

    push

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,8 @@
+// account
+export const GET_BALANCE = "/v1/Account/GetBalance";
 export const LOGIN = "/v1/Account/Login";
 export const REGISTER = "/v1/Account/Register";
+export const GET_PROFILE="/v1/Account/GetProfile"
 // support
 export const SUPPORT_SECTIONS_LIST = "/v1/Support/TicketSections";
 export const CREATE_TICKET = "/v1/Support/CreateTicket";
@@ -9,4 +12,4 @@ export const GET_SERVER_PLANS = "/v1/ServerPlan/GetServerPlanSelectList";
 // server location
 export const GET_SERVER_LOCATIONS =
   "/v1/ServerLocation/ServerLocationSelectList";
-export const GET_SERVER_LIST="/v1/Server/GetServerSelectList"
+export const GET_SERVER_LIST = "/v1/Server/GetServerSelectList";
--- src/components/avatar/ProfileBox.tsx
+++ src/components/avatar/ProfileBox.tsx
@@ -1,18 +1,55 @@
 import Avatar from "./Avatar";
 import Paragraph from "../typography/Paragraph";
 import { ColorType, Sizes } from "../../utils/enums";
+import { useQueries, useQuery } from "react-query";
+import { postMethod } from "../../api/callApi";
+import { GET_BALANCE, GET_PROFILE } from "../../api/endpoints";
+import { notify } from "../../utils/notify";
+import { numberWithCommas } from "../../utils/helper";
 
+const fetchBalance = async () => {
+  const response = await postMethod(GET_BALANCE, {});
+  if (response?.isSuccess) {
+    return response.data;
+  } else {
+    notify(response.message, "error");
+    return [];
+  }
+};
+const fetchProfile = async () => {
+  const response = await postMethod(GET_PROFILE, {});
+  if (response?.isSuccess) {
+    return response.data;
+  } else {
+    notify(response.message, "error");
+    return [];
+  }
+};
 const ProfileBox = () => {
+  const results = useQueries([
+    {
+      queryKey: "profile",
+      queryFn: fetchProfile,
+    },
+    {
+      queryKey: "balance",
+      queryFn: fetchBalance,
+    },
+  ]);
+
+  const { data: profile } = results[0];
+  const { data: balance } = results[1];
+
   return (
     <div className="flex items-center justify-center gap-2">
       <div className="text-left">
         <Paragraph size={Sizes.sm} className="font-medium">
-          مجید گرگین
+          {profile && profile.userName}
         </Paragraph>
         <Paragraph>
           <Paragraph size={Sizes.xs} type={ColorType.PRIMARY}>
             موجودی شما:
-            <span>۱۳,۰۰۰ تومان</span>
+            {balance && <span>{numberWithCommas(balance)} تومان</span>}
           </Paragraph>
         </Paragraph>
       </div>
