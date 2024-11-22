63723831196e46e087279bb6fb79bcc0648ae3dd
Majid <73994155+Majid2Dev@users.noreply.github.com>
2024-11-22 14:11:43 +0330

    add icon

--- src/pages/profile/MyProfile.tsx
+++ src/pages/profile/MyProfile.tsx
@@ -1,4 +1,8 @@
-import { UserIcon } from "@heroicons/react/24/outline";
+import {
+  LockClosedIcon,
+  PencilSquareIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import Tabs from "../../components/tabs/Tabs";
 import { TabType } from "../../components/tabs/TabsTypes";
 import UserProfile from "./_components/UserProfile/UserProfile";
@@ -6,27 +10,17 @@ import EditProfile from "./_components/EditProfile/EditProfile";
 import ChangePassword from "./_components/ChangePassword/ChangePassword";
 
 const MyProfile = () => {
-
-    const tabs: TabType[] = [
-        { icon: UserIcon, label: "پروفایل من", id: 1 },
-        { icon: UserIcon, label: "جزئیات حساب", id: 2 },
-        { icon: UserIcon, label: "تغییر کلمه عبور", id: 3 }
-
-    ]
-    const content = [
-        <UserProfile />,
-        <EditProfile />,
-        <ChangePassword />,
-
-    ]
-    return (
-
-        <div >
-            <Tabs tabs={tabs} componets={content}></Tabs>
-
-        </div>
-
-    );
-}
+  const tabs: TabType[] = [
+    { icon: UserIcon, label: "پروفایل من", id: 1 },
+    { icon: PencilSquareIcon, label: "جزئیات حساب", id: 2 },
+    { icon: LockClosedIcon, label: "تغییر کلمه عبور", id: 3 },
+  ];
+  const content = [<UserProfile />, <EditProfile />, <ChangePassword />];
+  return (
+    <div>
+      <Tabs tabs={tabs} componets={content}></Tabs>
+    </div>
+  );
+};
 
 export default MyProfile;
