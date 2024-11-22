1502017351bafd58c45fd205312532c3fdfaf82c
Majid <73994155+Majid2Dev@users.noreply.github.com>
2024-11-22 09:25:42 +0330

    backdrop-blur

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -14,22 +14,19 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <div
-      className={`fixed inset-0 z-[999] flex items-center justify-center ${
-        isOpen ? "translate-y-0" : "translate-y-[100%] delay-500"
-      }`}
+      className={`fixed inset-0 z-[999] backdrop-blur-[2px] flex items-center justify-center ${isOpen ? "translate-y-0" : "translate-y-[100%] delay-500"
+        }`}
     >
       <div
-        className={`absolute inset-0 z-[999] bg-[#0000009c] dark:bg-[#000c] transition-opacity duration-500 delay-100 ${
-          isOpen ? "opacity-100" : "opacity-0"
-        }`}
+        className={`absolute inset-0 z-[999] bg-[#0000009c] dark:bg-[#000c] transition-opacity duration-500 delay-100 ${isOpen ? "opacity-100" : "opacity-0"
+          }`}
         onClick={handleClose}
       ></div>
 
       <div
         className={`relative bg-white z-[9999] rounded-lg dark:border border-secondary-200 w-11/12 md:max-w-md mx-auto  overflow-y-auto
-        transition-all duration-300 delay-200 ${
-          isOpen ? "opacity-100" : "opacity-0"
-        }`}
+        transition-all duration-300 delay-200 ${isOpen ? "opacity-100" : "opacity-0"
+          }`}
       >
         <div className="py-4 px-6">
           <button
