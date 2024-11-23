fd0eb6b045fbdabbe4d96421b73cbb5dd63608b5
fatemehbd <baridaryan@gmail.com>
2024-11-24 01:05:02 +0330

    profile tab responsive

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -8,17 +8,17 @@ const Tabs: FC<TabsProps> = ({ tabs, componets }) => {
     <div>
 
       <div className="my-4 relative overflow-x-auto">
-        <ul className="inline-flex gap-2 bg-secondary-200 border border-border border-secondary-500 md:text-base text-xs rounded-md p-1">
+        <ul className="inline-flex sm:gap-2 bg-secondary-200 border border-border border-secondary-500 md:text-base text-xs rounded-md p-1">
           {tabs.map((tab) => (
             <li
               key={tab.id}
               className={`${selectedTab == tab.id
                 ? "bg-primary text-white  rounded-md"
                 : "text-secondary-700"
-                } flex items-center gap-1 py-2 px-3 cursor-pointer text-sm`}
+                }  flex text-nowrap items-center gap-1 py-2 px-3 cursor-pointer sm:text-sm text-xs`}
               onClick={() => setSelectedTab(tab.id)}
             >
-              <tab.icon className="w-5 h-5" />
+              <tab.icon className="md:size-5 size-4" />
               {tab.label}
             </li>
           ))}
